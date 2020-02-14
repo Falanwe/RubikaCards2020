c01911fdf985d89efc3e9489480a89c4b013ca0c
Jean-Noël Gourdol <jngourdol@stormancer.com>
2020-02-14 11:03:43 +0100

    Added need types to make the solution compile

---
+++ PlayingCards/Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayingCards
+{
+    public class Card : IComparable<Card>
+    {
+        public int CompareTo(Card secondCard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlayingCards/CardDealer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayingCards
+{
+    public class CardDealer
+    {
+        public CardDealer()
+        {
+        }
+
+        public void Shuffle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card DealOne()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
