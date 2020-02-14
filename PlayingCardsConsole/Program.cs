ae611ddab3a91c0db98898efccb197afce1eb328
Jean-Noël Gourdol <jngourdol@stormancer.com>
2020-02-14 10:06:34 +0100

    Creation of the basic solution layout

---
+++ PlayingCardsConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlayingCardsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dealer = new CardDealer();
+
+            dealer.Shuffle();
+
+            var firstCard = dealer.DealOne();
+            var secondCard = dealer.DealOne();
+
+            Console.WriteLine($"First card ({firstCard}) is {(firstCard.CompareTo(secondCard) > 0 ? "greater" : "smaller")} than second card ({secondCard}.");
+        }
+    }
+}
