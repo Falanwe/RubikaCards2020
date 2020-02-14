09972d1cd37a7f309968825f03c5b146a675b946
LHouyez <l.houyez@rubika-edu.com>
2020-02-14 10:54:32 +0100

    Compile

---
+++ PlayingCards/CardDealer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public class CardDealer
+    {
+        public enum CardType {Spades,Hearts,Diamonds,Clubs }
+        private CardType myType;
+
+        public CardType MyType { get => myType; set => myType = value; }
+
+        public CardDealer()
+        {
+            Random rand = new Random();
+            rand.Next(0, 4);
+          //  MyType = rand;
+
+        }
+
+        public void Shuffle()
+        {
+
+
+
+        }
+
+        public CardDealer DealOne()
+        {
+            return this;
+        }
+
+        public int CompareTo(CardDealer cardToCompare)
+        {
+            return 0;
+        }
+
+    }
+}
