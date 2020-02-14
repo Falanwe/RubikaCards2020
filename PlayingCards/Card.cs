aebe74ead70711eaa93b8547127d7568bd20d685
LHouyez <l.houyez@rubika-edu.com>
2020-02-14 14:03:52 +0100

    modif cards

--- PlayingCards/Card.cs
+++ PlayingCards/Card.cs
@@ -3,21 +3,30 @@
 namespace PlayingCards
 {
 
-
     public class Card : IComparable<Card>
     {
 
-        CardColor myColor;
-        CardValue myValue;
-        public CardColor MyColor { get => myColor; set => myColor = value; }
-        public CardValue MyValue { get => myValue; set => myValue = value; }
+        public CardColor MyColor { get; set; }
+        public CardValue MyValue { get; set; }
+        public CardType MyType
+        {
+            get
+            {
+                var isRed = (MyColor == CardColor.Diamonds)|| (MyColor == CardColor.Hearts);
+                var isHead = (MyValue >= CardValue.Jack);
+
+                // "|" permet d'additionner des bytes et donc de merge différentes valeurs de l'enum
+                return (isRed ? CardType.Red : CardType.Balck) | (isHead ? CardType.Heads : CardType.Number);
+            }
+            
+        }
 
         public Card()
         {
             Random rand = new Random();
             MyColor = (CardColor)rand.Next(0, 4);
             Random rand2 = new Random();
-            MyValue = (CardValue)rand2.Next(0, 14);
+            MyValue = (CardValue)rand2.Next(2, 15);
         }
 
 
@@ -46,7 +55,9 @@ namespace PlayingCards
             //   throw new NotImplementedException();
         }
     }
+    public enum CardColor { Spades, Diamonds, Hearts, Clubs }
+    public enum CardValue { Two = 2, Three, Four, Five, Six, Seven, Heigh, Nine, Ten, Jack, Queen, King, Ace }
+    [Flags]
+    public enum CardType { Red = 1, Balck = 2, Heads = 4, Number = 8 }
+    // 0x = hexadécimal  ,0x1 0x2 0x4 0x8 0x10 0x20
 }
-
-public enum CardColor { Spades, Diamonds, Hearts, Clubs }
-public enum CardValue { Ace, Two, Three, Four, Five, Six, Seven, Heigh, Nine, Ten, Jack, Queen, King }
