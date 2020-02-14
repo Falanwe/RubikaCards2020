7ab1fccd033381cccf6057e206284591afedb799
Slean <s.dubuc@rubika-edu.com>
2020-02-14 10:54:47 +0100

    Dealer

---
+++ PlayingCards/Card.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public class Card : IComparable<Card>
+    {
+        public enum CardColor
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+
+        public enum CardValue
+        {
+            Two = 2,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
+
+        public CardColor color;
+        public CardValue value;
+
+
+
+        public int CompareTo(Card other)
+        {
+            if(other.value > value)
+            {
+                return 1;
+            }
+            else if(other.value < value)
+            {
+                return -1;
+            }
+            else
+            {
+                if(other.color > color)
+                {
+                    return 1;
+                }
+                else if(other.color < color)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        }
+    }
+}
---
+++ PlayingCards/CardDealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public class CardDealer
+    {
+        public void Shuffle()
+        {
+
+        }
+
+        public Card DealOne()
+        {
+            Card newCard = new Card();
+            Random rand = new Random();
+            newCard.color = (Card.CardColor)rand.Next(0, 4);
+            newCard.value = (Card.CardValue)rand.Next(2, 16);
+
+            return newCard;
+        }
+
+
+    }
+
+
+}
