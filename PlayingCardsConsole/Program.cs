50953bd20d20ade4436c616c2d4e335bdfa41699
Nicolas <nicoceriani@gmail.com>
2020-02-14 10:55:51 +0100

    First draft

--- PlayingCardsConsole/Program.cs
+++ PlayingCardsConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlayingCardsConsole
 {
@@ -7,18 +8,80 @@ namespace PlayingCardsConsole
         static void Main(string[] args)
         {
             var dealer = new CardDealer();
-
+            dealer.Initialize();
             dealer.Shuffle();
 
-            var firstCard = dealer.DealOne();
-            var secondCard = dealer.DealOne();
-
-            Console.WriteLine($"First card ({firstCard}) is {(firstCard.CompareTo(secondCard) > 0 ? "greater" : "smaller")} than second card ({secondCard}.");
+            Card firstCard = dealer.DealOne();
+            Card secondCard = dealer.DealOne();
+            string compare = firstCard.CompareTo(secondCard);
+            Console.WriteLine(compare);
         }
     }
 
     public class CardDealer
     {
+        private List<Card> cards = new List<Card>();
+        
+        public void Initialize()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int t = 0; t < 14; t++)
+                {
+                    Card card = new Card((Suit)i, t);
+                    cards.Add(card);
+                }
+            }
+        }
 
+        public void Shuffle()
+        {
+
+        }
+
+        public Card DealOne()
+        {
+            int index = 0;
+            Card card = cards[index];
+            cards.Remove(card);
+            return card;
+        }
+    }
+
+    public class Card
+    {
+        public Suit suit;
+        public int rank;
+
+        public Card(Suit suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+        public string GetName()
+        {
+            return suit.ToString() + " " + rank;
+        }
+
+        public string CompareTo(Card otherCard)
+        {
+            string result = GetName() + " is";
+            int rankA = rank;
+            int rankB = otherCard.rank;
+            if (rankA > rankB) result += " greater than ";
+            else if (rankA < rankB) result += " less than ";
+            else result += " equal to ";
+            result += otherCard.GetName();
+            return result;
+        }
+    }
+
+    public enum Suit
+    {
+        Spade,
+        Club,
+        Diamond,
+        Heart
     }
 }
