85e165b31c1588872968a05799ab27cb93854abe
Slean <s.dubuc@rubika-edu.com>
2020-02-19 14:37:06 +0100

    Fibo

---
+++ PlayingCards/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCards
+{
+    public static class Fibonacci
+    {
+        public static long Fibo(int len)
+        {
+            long a = 0, b = 1, c = 0;
+            for (int i = 1; i < len; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+}
--- PlayingCardsConsole/Program.cs
+++ PlayingCardsConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using PlayingCards;
+using System.Collections.Generic;
+
 
 
 namespace PlayingCardsConsole
@@ -12,10 +14,20 @@ namespace PlayingCardsConsole
 
             dealer.Shuffle();
 
-            var firstCard = dealer.DealOne();
-            var secondCard = dealer.DealOne();
+            //var firstCard = dealer.DealOne();
+            //var secondCard = dealer.DealOne();
 
-            Console.WriteLine($"First card ({firstCard}) is {(firstCard > secondCard ? "greater" : "smaller")} than second card ({secondCard}).");
+            //Console.WriteLine($"First card ({firstCard}) is {(firstCard > secondCard ? "greater" : "smaller")} than second card ({secondCard}).");
+
+            Dictionary<string, List<Card>> playerHands = new Dictionary<string, List<Card>>();
+
+            playerHands.Add("Alexis", new List<Card>
+            {
+                new Card(CardColor.Hearts, CardValue.Seven),
+                new Card(CardColor.Clubs, CardValue.King)
+            });
+
+            Console.Write(Fibonacci.Fibo(80).ToString("NO"));
         }
     }
 }
