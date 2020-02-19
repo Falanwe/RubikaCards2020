be2281378e664f16094064f9a3a7a780ea0b83f6
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2020-02-19 14:37:35 +0100

    Fibonacci

---
+++ PlayingCardsConsole/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public static class Fibonacci
+    {
+        public static double Fibo(int n)
+        {
+            double a = 0;
+            double b = 1;
+            double result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+
+            return a;
+        }
+    }
+}
--- PlayingCardsConsole/Program.cs
+++ PlayingCardsConsole/Program.cs
@@ -6,14 +6,7 @@ namespace PlayingCardsConsole
     {
         static void Main(string[] args)
         {
-            var dealer = new CardDealer();
-
-            dealer.Shuffle();
-
-            var firstCard = dealer.DealOne();
-            var secondCard = dealer.DealOne();
-
-            Console.WriteLine($"First card ({firstCard}) is {(firstCard.CompareTo(secondCard) > 0 ? "greater" : "smaller")} than second card ({secondCard}.");
+            Console.WriteLine(Fibonacci.Fibo(80));
         }
     }
 }
