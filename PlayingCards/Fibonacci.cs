2b26ae3f3532bcb57a4a0ede4cd70872b6001be7
Kelda <sebastien.butor@orange.fr>
2020-02-19 14:37:13 +0100

    Fibonacci

---
+++ PlayingCards/Fibonacci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace PlayingCards
+{
+    public class Fibonacci
+    {
+        public static int FiboNaive(int n)
+        {
+            switch (n)
+            {
+                case (0):
+                    return 1;
+                case (1):
+                    return 1;
+                default:
+                    return FiboNaive(n-2) + FiboNaive(n-1);
+            }
+        }
+
+        public static BigInteger FiboList(int n)
+        {
+            //List<BigInteger> values = new List<BigInteger> { 0, 1 };
+            //for (int i = 2; i <= n; i++)
+            //{
+            //    values.Add(values[i - 1] + values[i - 2]);
+            //}
+            //return values[n];
+            BigInteger downValue = 0;
+            BigInteger upValue = 1;
+            BigInteger stock = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                stock = upValue;
+                upValue = downValue + upValue;
+                downValue = stock;
+                //values.Add(values[i - 1] + values[i - 2]);
+            }
+
+            return n == 0 ? downValue : upValue;
+        }
+    }
+}
--- PlayingCardsConsole/Program.cs
+++ PlayingCardsConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using PlayingCards;
-
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace PlayingCardsConsole
 {
@@ -12,10 +13,67 @@ namespace PlayingCardsConsole
 
             dealer.Shuffle();
 
-            var firstCard = dealer.DealOne();
-            var secondCard = dealer.DealOne();
+            //var firstCard = dealer.DealOne();
+            //var secondCard = dealer.DealOne();
+            //
+            //Console.WriteLine($"First card ({firstCard}) is {(firstCard > secondCard ? "greater" : "smaller")} than second card ({secondCard}).");
 
-            Console.WriteLine($"First card ({firstCard}) is {(firstCard > secondCard ? "greater" : "smaller")} than second card ({secondCard}).");
+            Dictionary<string, List<Card>> playerHands = new Dictionary<string, List<Card>>();
+
+            playerHands.Add("Alexis", new List<Card>
+            {
+                new Card(CardColor.Spades, CardValue.Ace),
+                new Card(CardColor.Diamonds, CardValue.Two)
+            });
+
+            playerHands.Add("François", new List<Card>
+            {
+                new Card(CardColor.Hearts, CardValue.Seven),
+                new Card(CardColor.Clubs, CardValue.King)
+            });
+
+            /*foreach(var value in playerHands.Values)
+            {
+                foreach (var card in value)
+                {
+                    Console.WriteLine(card);
+                }
+
+            }*/
+
+            #region Exactement la même chose que d'appeler un foreach
+            /*var hand = playerHands["Alexis"];
+            IEnumerator<Card> enumerator = null;
+            try
+            {
+                enumerator = hand.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+                }
+            }
+            finally
+            {
+                enumerator.Dispose();
+            }*/
+            #endregion
+
+            #region ConcurrentBag --> permet de modifier l'énumérable pendant son parcours 
+            /*ConcurrentBag<Card> hand = new ConcurrentBag<Card>(playerHands["Alexis"]);
+
+            foreach(var card in hand)
+            {
+                Console.WriteLine(card);
+                hand.TryTake(out var newCard);
+            }
+            Console.WriteLine(hand.Count);*/
+            #endregion
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine(Fibonacci.FiboList(i));
+            }
+            Console.WriteLine(Fibonacci.FiboList(100000));
         }
     }
 }
