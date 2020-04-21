6aa9fea819d2d7784f4e3ed26673a504bca6fb7d
Jean-Noël Gourdol <jngourdol@stormancer.com>
2020-04-21 10:27:12 +0200

    Adds ISort interface

---
+++ PlayingCardsConsole/ISort.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public interface ISort
+    {
+        IEnumerable<T> Sort<T>(IEnumerable<T> source) where T : IComparable<T>;
+    }
+}
