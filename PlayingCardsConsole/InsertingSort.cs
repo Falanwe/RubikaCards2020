7966c16a0e93b17db4c2e62b76b4ea7420dcb838
Jean-Noël Gourdol <jngourdol@stormancer.com>
2020-04-21 14:07:52 +0200

    Adds insert sort implementation

---
+++ PlayingCardsConsole/InsertingSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayingCardsConsole
+{
+    public class InsertingSort : ISort
+    {
+        public IEnumerable<T> Sort<T>(IEnumerable<T> source) where T : IComparable<T>
+        {
+            var copy = source.ToArray();
+
+            void Swap(int i, int j)
+            {
+                var temp = copy[i];
+                copy[i] = copy[j];
+                copy[j] = temp;
+            }
+
+            for(var insertingIndex = 0; insertingIndex<copy.Length; insertingIndex++)
+            {
+                for(var comparingIndex = insertingIndex +1; comparingIndex < copy.Length; comparingIndex++)
+                {
+                    if(copy[insertingIndex].CompareTo(copy[comparingIndex])>0)
+                    {
+                        Swap(insertingIndex, comparingIndex);
+                    }
+                }
+            }
+
+            return copy;
+        }
+    }
+}
